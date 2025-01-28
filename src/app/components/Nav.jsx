155a30ab5ad4dfca1a2cdcc1f769a10c98c78cf8
Gwerh <gwerh.dev@gmail.com>
2025-01-27 21:46:16 -0300

    updating nav

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,41 +1,39 @@
 import s from "./Nav.module.css";
+import { faBars, faBurger, faHamburger, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Logo } from "./Logo";
-import { PrimaryButton } from "./Buttons/PrimaryButton";
-import { DialogModal } from "./Modals/DialogModal";
-import { useState } from "react";
-import { LoginForm } from "./Forms/LoginForm";
+import { DEVELOPERS_URL } from "../../middlewares/config";
 
 export const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpenLoginDialog = () => {
-    setIsOpen(true);
-  };
-
-  const handleCloseLoginDialog = () => {
-    setIsOpen(false);
-  };
-
   return (
     <div className={s.refNav}>
       <section className={s.navSectionCont}>
         <div className={s.navCont}>
           <div className={s.divNav}>
-            <ul className={s.ulNav}>
+            <span className={s.burgerContainer}>
+              <FontAwesomeIcon size="xl" icon={faBars} />
+            </span>
+
+            <span className={s.logoContainer}>
               <Logo height={"65px"} />
-            </ul>
+            </span>
+
             <ul className={s.ulNav}>
 
             </ul>
             <ul className={s.ulNav}>
-              <PrimaryButton onClick={handleOpenLoginDialog} text="Ingresar" />
+              <li>
+                <a className={s.aNav} href={DEVELOPERS_URL} text="Ingresar">
+                  <FontAwesomeIcon size="xl" icon={faUser} />
+                  <span>
+                    Ingresar
+                  </span>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </section>
-      <DialogModal isOpen={isOpen} onClose={handleCloseLoginDialog} >
-        <LoginForm />
-      </DialogModal>
     </div>
   )
 }
--- src/middlewares/config/index.js
+++ src/middlewares/config/index.js
@@ -2,3 +2,4 @@ import { environment } from "../../environment";
 import { DEVELOPMENT } from "../misc/consts";
 
 export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080":"https://developers-terminalcore-api.fly.dev";
+export const DEVELOPERS_URL = "https://developers-terminalcore.vercel.app";
