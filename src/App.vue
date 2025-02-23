10088c1f0b3fb9423567aa9a91dfaae2d6f620e9
GwerhDev <gwerh.dev@gmail.com>
2025-02-23 12:10:41 -0300

    styling globals

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@ import NavBar from './app/components/Navigator/NavBar.component.vue';
 </script>
 
 <template>
+  <header>
+    <nav-bar />
+  </header>
   <div class="viewport-container">
-    <header>
-      <nav-bar />
-    </header>
     <div class="app-container">
       <div class="router-view">
         <router-view />
